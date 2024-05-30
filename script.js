45c0e06186824c68a24ce962a8734ed5cb0edcb5
135791maria <mjosedossantosmarques92@gmail.com>
2024-05-30 16:08:52 -0300

    trabalho landpage

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+function trocaCor(cor){
+let circulo = document.querySelector(".circulo")
+circulo.style.background = cor
+
+}
+function trocaImage(imagem){
+let imgIphone = document.querySelector(".iphone")
+imgIphone.src = imagem
+}
